95b77f3fb879e5e0e91c05e752e8d8580e83c8af
Amber Jones <amberlovescats14@gmail.com>
2019-12-18 16:42:01 -0600

    finished order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -12,7 +12,10 @@ order by first_name , last_name;
 select * from employees where first_name IN('Irena', 'Vidya', 'Maya')
 order by last_name , first_name;
 
-select * from employees where LOWER(last_name) like 'e%';
+
+# // DESC
+select * from employees where LOWER(last_name) like 'e%'
+order by emp_no desc, last_name desc;
 
 
 select * from employees where hire_date
@@ -39,7 +42,8 @@ select * from employees where last_name like '%e'
 
 select * from employees where
 (month(birth_date)=12 and day(birth_date)=25)
-and hire_date between '1989-12-31' and '2000-01-01';
+and hire_date between '1989-12-31' and '2000-01-01'
+order by birth_date, hire_date desc;
 
 select * from employees where last_name like '%q%'
                           and last_name not like '%qu%';
